19984b083bf3500614a2101adc548cf948d17016
Jens Kürten <webmaster@jenskuerten.de>
2024-06-04 07:47:00 +0200

    Improve versioning (#8)
    * add __version__
    
    * ensure published version matches the tag
    
    ---------
    
    Co-authored-by: Jens Kürten <jens.kuerten@contact-software.com>

--- csfunctions/__init__.py
+++ csfunctions/__init__.py
@@ -1,3 +1,7 @@
+import importlib.metadata
+
+__version__ = importlib.metadata.version("contactsoftware-functions")
+
 from .events import Event
 from .metadata import MetaData
 from .request import Request
