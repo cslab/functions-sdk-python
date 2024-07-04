e8be26fb47c7376b3770e6ffd1f781211857563b
Jens Kürten <jens.kuerten@contact-software.com>
2024-07-04 15:10:53 +0200

    add release check events for documents, parts and ecs

--- csfunctions/events/base.py
+++ csfunctions/events/base.py
@@ -6,12 +6,13 @@ from pydantic import BaseModel, Field
 class EventNames(str, Enum):
     DUMMY = "dummy"
     DOCUMENT_RELEASE = "document_release"
+    DOCUMENT_RELEASE_CHECK = "document_release_check"
     PART_RELEASE = "part_release"
+    PART_RELEASE_CHECK = "part_release_check"
     ENGINEERING_CHANGE_RELEASE = "engineering_change_release"
+    ENGINEERING_CHANGE_RELEASE_CHECK = "engineering_change_release_check"
     FIELD_VALUE_CALCULATION = "field_value_calculation"
     WORKFLOW_TASK_TRIGGER = "workflow_task_trigger"
-    RELEASE_CHECK = "release_check"
-    RELEASED = "released"
 
 
 class BaseEvent(BaseModel):
