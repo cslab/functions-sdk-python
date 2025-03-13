35d68ec64321375ef2502db145976dd444c6c05c
Jens Kürten <webmaster@jenskuerten.de>
2025-03-13 15:42:33 +0100

    Feat: Add create and modify check events for docs and parts (#18)
    * feat: Add new events for document and part checks
    ---------
    
    Co-authored-by: Jens Kürten <jens.kuerten@contact-software.com>

--- csfunctions/events/__init__.py
+++ csfunctions/events/__init__.py
@@ -3,12 +3,16 @@ from typing import Annotated, Union
 from pydantic import Field
 
 from .dialog_data import DocumentReleaseDialogData, PartReleaseDialogData
+from .document_create_check import DocumentCreateCheckData, DocumentCreateCheckEvent
+from .document_modify_check import DocumentModifyCheckData, DocumentModifyCheckEvent
 from .document_release import DocumentReleaseData, DocumentReleaseEvent
 from .document_release_check import DocumentReleaseCheckData, DocumentReleaseCheckEvent
 from .dummy import DummyEvent, DummyEventData
 from .engineering_change_release import EngineeringChangeRelease, EngineeringChangeReleaseData
 from .engineering_change_release_check import EngineeringChangeReleaseCheck, EngineeringChangeReleaseCheckData
 from .field_value_calculation import FieldValueCalculationData, FieldValueCalculationEvent
+from .part_create_check import PartCreateCheckData, PartCreateCheckEvent
+from .part_modify_check import PartModifyCheckData, PartModifyCheckEvent
 from .part_release import PartReleaseData, PartReleaseEvent
 from .part_release_check import PartReleaseCheckData, PartReleaseCheckEvent
 from .workflow_task_trigger import WorkflowTaskTriggerEvent, WorkflowTaskTriggerEventData
@@ -24,6 +28,10 @@ Event = Annotated[
         EngineeringChangeRelease,
         EngineeringChangeReleaseCheck,
         WorkflowTaskTriggerEvent,
+        DocumentCreateCheckEvent,
+        DocumentModifyCheckEvent,
+        PartCreateCheckEvent,
+        PartModifyCheckEvent,
     ],
     Field(discriminator="name"),
 ]
@@ -37,6 +45,10 @@ EventData = Union[
     EngineeringChangeReleaseData,
     EngineeringChangeReleaseCheckData,
     WorkflowTaskTriggerEventData,
+    DocumentCreateCheckData,
+    DocumentModifyCheckData,
+    PartCreateCheckData,
+    PartModifyCheckData,
 ]
 
 __all__ = [
@@ -60,4 +72,12 @@ __all__ = [
     "WorkflowTaskTriggerEventData",
     "DocumentReleaseDialogData",
     "PartReleaseDialogData",
+    "DocumentCreateCheckData",
+    "DocumentCreateCheckEvent",
+    "DocumentModifyCheckData",
+    "DocumentModifyCheckEvent",
+    "PartCreateCheckData",
+    "PartCreateCheckEvent",
+    "PartModifyCheckData",
+    "PartModifyCheckEvent",
 ]
